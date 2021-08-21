4cac269933404f66f9f26b28913fcf8f2d220b7e
y2000computer <y2000computer@gmail.com>
2021-08-22 01:51:33 +0800

    update

--- public_html/downloadFile.php
+++ public_html/downloadFile.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
-require_once __DIR__.'/../env.php';  
+//require_once __DIR__.'/../env.php';  
+DEFINE('DIR_PUBLIC_HTML', __DIR__.'/../document');
+
 
 
 if(!$_SESSION["sUserID"]) {
