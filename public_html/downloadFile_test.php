7d485876c6284b2c667fc5e927a509025048318a
y2000computer <y2000computer@gmail.com>
2021-08-22 01:17:04 +0800

    update

---
+++ public_html/downloadFile_test.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+require_once __DIR__.'/../env.php';  
+
+require_once __DIR__.'/../app/photolibrary/photolibrary_autoload_config.php';  
+require_once __DIR__.'/../app/function/framework_func.php';
+require_once __DIR__.'/../app/function/date_func.php';
+require_once __DIR__.'/../app/function/sql_func.php';
+require_once __DIR__.'/../app/function/string_func.php';
+require_once __DIR__.'/../app/function/translate_func.php';  
+
+if(!$_SESSION["sUserID"]) {
+	echo 'no session<br>';
+	exit;
+}
+?>
+<?php
+		//$filename ='012345678_1.jpg';
+		//$folder ='driver_upload/2016';
+		$filename =$_GET['filename'];
+		$path =$_GET['path'];
+		$full_path =DIR_PUBLIC_HTML.'/'.$path.'/'.$filename;
+		echo 'full_path ='.$full_path.'<br>';
+		die();
+?>
+
+
